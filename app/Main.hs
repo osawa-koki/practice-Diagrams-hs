b64643c376dde19603f72fc2cd6cc289dcbfd5b0
osawa-koki <osawa-koki@osawa.dev>
2022-10-26 14:51:08 +0000

    Apollonianまで。

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,22 @@
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
+import Diagrams.TwoD.Apollonian
 
-myCircle :: Diagram B
-myCircle = circle 1
 
-main = mainWith myCircle
+-- -- 単純な円を描写
+-- -- $ stack exec -- practice-Diagrams-hs-exe -o ./fruits/myCircle.svg -w 400
+-- -- https://archives.haskell.org/projects.haskell.org/diagrams/doc/quickstart.html#your-first-diagram
+
+-- myCircle :: Diagram B
+-- myCircle = circle 1
+-- main = mainWith myCircle
+
+
+-- -- apollonianGasketを描写
+-- -- $ stack exec -- practice-Diagrams-hs-exe -o ./fruits/apollonianGasket.svg -w 400
+-- -- https://archives.haskell.org/projects.haskell.org/diagrams/gallery/Apollonian.html
+
+-- example = (apollonianGasket 0.01 2 3 3)
+-- main = mainWith (example :: Diagram B)
+
+
